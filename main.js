f212a747b6132af7422faa009556192cfcbce012
Manoj <manojjeeramani@gmail.com>
2023-01-08 21:15:28 -0600

    Added a few more pages

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
-
+function fallback(video)
+    {
+      const img = video.querySelector('fallback-img');
+      if (img)
+        video.parentNode.replaceChild(img, video);
+    }
+    const video = document.getElementById('bg-video')
+    video.muted = true
+    video.addEventListener('loadeddata', function (){
+      video.play()
+    });
 
 
 
